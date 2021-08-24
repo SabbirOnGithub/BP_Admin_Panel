f48687e5dbde3d8e6280c43a5ce19cdb20737659
Sabbir <sabbir.ahmed.dev@gmail.com>
2021-08-25 01:48:24 +0600

    ok

--- src/screens/dashboardScreens/subMenuOverViewScreen/SubMenuOverViewForm.js
+++ src/screens/dashboardScreens/subMenuOverViewScreen/SubMenuOverViewForm.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react'
 import { Grid } from '@material-ui/core';
-import Controls from "../../../components/controls/Controls";
-import { useForm, Form } from '../../../components/UseForm/useForm';
-import { EditorState, ContentState, convertToRaw  } from 'draft-js';
-import htmlToDraft from 'html-to-draftjs';
+import { ContentState, convertToRaw, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import React, { useEffect } from 'react';
+import Controls from "../../../components/controls/Controls";
+import { Form, useForm } from '../../../components/UseForm/useForm';
 
 
 const initialFValues = {
     id: '',
     subMenuId:'',
     title: '',
-    description: EditorState.createEmpty(),
+    description: '',
     pictureUrl: '',
 }
 
@@ -40,7 +40,6 @@ export default function SubMenuOverViewForm(props) {
         handleInputChange,
         handleFileChange,
         resetForm,
-        handleEditorInput,
         resetFileInput
     } = useForm(initialFValues, true, validate);
 
@@ -102,11 +101,18 @@ export default function SubMenuOverViewForm(props) {
                         onChange={handleInputChange}
                         error={errors.title}
                     />
-                    <Controls.RichTextEditor
+                    <Controls.Input
+                        label="Description"
+                        name="description"
+                        value={values.description}
+                        onChange={handleInputChange}
+                        error={errors.description}
+                    />
+                    {/* <Controls.RichTextEditor
                         onEditorStateChange={value => handleEditorInput('description', value)} //handleEditorInput(name, value)
                         placeholder="Description here..."
                         editorState = {values.description}
-                    />
+                    /> */}
                     <Controls.FileInput
                         name="pictureUrl"
                         label="Picture"
