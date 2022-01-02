1a5278b5b4d2f1fdbb6dccd820c63870a4721588
Sabbir Ahmed <sabbir.ahmed.dev@gmail.com>
2022-01-02 19:58:25 +0600

    ok

--- src/screens/dashboardScreens/ctaScreen/ctaFunctionScreen/CtaFormStepFive.js
+++ src/screens/dashboardScreens/ctaScreen/ctaFunctionScreen/CtaFormStepFive.js
@@ -1,6 +1,10 @@
 import { Grid } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import {
+  CardElement, Elements, useElements, useStripe
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect } from "react";
 import PaypalExpressBtn from "react-paypal-express-checkout";
 import StripeCheckout from "react-stripe-checkout";
@@ -15,6 +19,62 @@ const {
   REACT_APP_PAYPAL_PRODUCTION_APP_ID,
   REACT_APP_PAYPAL_ENV,
 } = config;
+
+
+const stripePromise = loadStripe("pk_test_51JH9oLJ708iVAZsoxMt8YdGnYBxxyAbI65tct1LbKy5kWenz6E8CTzJlWr4eOnADslmwaqfTMVqiyZKvlHYJdxX800JOYkQTcB");
+
+const handleSubmit = (stripe, elements) => async () => {
+  const cardElement = elements.getElement(CardElement);
+
+  const {error, paymentMethod} = await stripe.createPaymentMethod({
+    type: 'card',
+    card: cardElement,
+  });
+
+  if (error) {
+    console.log('[error]', error);
+  } else {
+    console.log('[PaymentMethod]', paymentMethod);
+    // ... SEND to your API server to process payment intent
+  }
+};
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  return (
+    <>
+      <h1>stripe form</h1>
+      <CardElement />
+      <button onClick={handleSubmit(stripe, elements)}>Buy</button>
+    </>
+  );
+}
+
+const appearance = {
+  theme: 'stripe',
+};
+const clientSecret = {
+  clientSecret: 'sk_test_51JH9oLJ708iVAZsoFVngRW8H9UTSd9RXQv2tScPoNhdkQ1mqZ3Jw7Mb0JOGKxhDqPcfytGejEz1ZxJOo6sEmlWVy00NVHupHYu',
+};
+
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -131,65 +191,23 @@ export default function CtaFormStepFive(props) {
       ) : (
         <Form>
           <Grid container>
-            <Grid item xs={12}>
-              <h1 style={{ alignSelf: "center", textAlign: "center" }}>
-                {" "}
-                Amount : USD {amountString}{" "}
-              </h1>
-
+            <Grid item xs={6}>
+            <h1 style={{ alignSelf: "center", textAlign: "center" }}>
+                        Packege Name : {createOrder?.name}
+                      </h1> 
+                      <h1 style={{ alignSelf: "center", textAlign: "center" }}>
+                          Amount : USD {amountString}{" "}
+                      </h1>
+                    {/* <pre>
+                    createOrder : {JSON.stringify(createOrder, undefined, 4)}
+                  </pre>
+                  <pre>values: {JSON.stringify(values, undefined, 4)}</pre> */}
+            </Grid>
+            <Grid item xs={6}>
               <div className={classes.paymentArea}>
-                {/* <pre>
-                  createOrder : {JSON.stringify(createOrder, undefined, 4)}
-                </pre>
-                <pre>values: {JSON.stringify(values, undefined, 4)}</pre> */}
-                <div>
-                  {createOrder?.rate && (
-                    <StripeCheckout
-                      stripeKey={REACT_APP_STRIPE_KEY}
-                      token={(token) =>
-                        handleCtaPayment(token, createOrder, setActiveStep)
-                      }
-                      amount={amount}
-                      name="Best Practicify, LLC"
-                      billingAddress
-                      description={paymentDesc}
-                      locale="auto"
-                      currency="USD"
-                      image="https://www.bestpracticify.co/BP_logo.svg"
-                    >
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        // startIcon={<img src={process.env.PUBLIC_URL+"/stripe_icon.png"}  height='25px' width='25px' alt='stripe logo' />}
-                        // endIcon={<InputSharpIcon>Purchase</InputSharpIcon>}
-                        endIcon={
-                          <svg
-                            viewBox="0 0 60 25"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="60"
-                            height="25"
-                            className="UserLogo variant-- "
-                          >
-                            <title>Stripe logo</title>
-                            <path
-                              fill="var(--userLogoColor, #fff)"
-                              d="M59.64 14.28h-8.06c.19 1.93 1.6 2.55 3.2 2.55 1.64 0 2.96-.37 4.05-.95v3.32a8.33 8.33 0 0 1-4.56 1.1c-4.01 0-6.83-2.5-6.83-7.48 0-4.19 2.39-7.52 6.3-7.52 3.92 0 5.96 3.28 5.96 7.5 0 .4-.04 1.26-.06 1.48zm-5.92-5.62c-1.03 0-2.17.73-2.17 2.58h4.25c0-1.85-1.07-2.58-2.08-2.58zM40.95 20.3c-1.44 0-2.32-.6-2.9-1.04l-.02 4.63-4.12.87V5.57h3.76l.08 1.02a4.7 4.7 0 0 1 3.23-1.29c2.9 0 5.62 2.6 5.62 7.4 0 5.23-2.7 7.6-5.65 7.6zM40 8.95c-.95 0-1.54.34-1.97.81l.02 6.12c.4.44.98.78 1.95.78 1.52 0 2.54-1.65 2.54-3.87 0-2.15-1.04-3.84-2.54-3.84zM28.24 5.57h4.13v14.44h-4.13V5.57zm0-4.7L32.37 0v3.36l-4.13.88V.88zm-4.32 9.35v9.79H19.8V5.57h3.7l.12 1.22c1-1.77 3.07-1.41 3.62-1.22v3.79c-.52-.17-2.29-.43-3.32.86zm-8.55 4.72c0 2.43 2.6 1.68 3.12 1.46v3.36c-.55.3-1.54.54-2.89.54a4.15 4.15 0 0 1-4.27-4.24l.01-13.17 4.02-.86v3.54h3.14V9.1h-3.13v5.85zm-4.91.7c0 2.97-2.31 4.66-5.73 4.66a11.2 11.2 0 0 1-4.46-.93v-3.93c1.38.75 3.1 1.31 4.46 1.31.92 0 1.53-.24 1.53-1C6.26 13.77 0 14.51 0 9.95 0 7.04 2.28 5.3 5.62 5.3c1.36 0 2.72.2 4.09.75v3.88a9.23 9.23 0 0 0-4.1-1.06c-.86 0-1.44.25-1.44.9 0 1.85 6.29.97 6.29 5.88z"
-                              fillRule="evenodd"
-                            ></path>
-                          </svg>
-                        }
-                        size="medium"
-                        // color="inherit"
-                      >
-                        <span className="paypal-button-text" optional="">
-                          Pay with{" "}
-                        </span>
-                      </Button>
-                    </StripeCheckout>
-                  )}
-                </div>
-                <div style={{ marginTop: 15 }}>
+
+
+              <div >
                   <PaypalExpressBtn
                     env={REACT_APP_PAYPAL_ENV}
                     client={client}
@@ -207,6 +225,44 @@ export default function CtaFormStepFive(props) {
                     style={stylePaypal}
                   />
                 </div>
+
+                
+
+                <div style={{marginTop:15, width:"45%", }}>
+                  <Elements stripe={stripePromise}>
+                      <PaymentForm />
+                    </Elements>
+                </div>
+
+                <div style={{ marginTop: 15 }}>
+                  {createOrder?.rate && (
+                    <StripeCheckout
+                      stripeKey={REACT_APP_STRIPE_KEY}
+                      token={(token) =>
+                        handleCtaPayment(token, createOrder, setActiveStep)
+                      }
+                      amount={amount}
+                      name="Best Practicify"
+                      billingAddress
+                      description={paymentDesc}
+                      locale="auto"
+                      currency="USD"
+                      image="https://www.bestpracticify.co/images/BP_logo_Straight.png"
+                    >
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                      >
+                        <span className="paypal-button-text" optional="" style={{color:"#fff"}}>
+                          Pay with Card
+                        </span>
+                      </Button>
+                    </StripeCheckout>
+                  )}
+                </div>
+
+                
               </div>
             </Grid>
           </Grid>
